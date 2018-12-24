057b61fa2233e32ae2660c74b0ab23ed3df5eaea
LUWEI <REEDLUWEI1993@163.com>
2018-12-24 15:06:03 +0800

    12.241233

--- src/main/java/com/nanjing/weather/Application.java
+++ src/main/java/com/nanjing/weather/Application.java
@@ -12,6 +12,6 @@ import org.springframework.context.annotation.Import;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        //芦苇啥傻蛋
+        //
     }
 }
