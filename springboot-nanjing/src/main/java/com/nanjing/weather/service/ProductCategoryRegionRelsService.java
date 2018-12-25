72e89fc9f148d9111dbe52fdb1102f4414266b16
LUWEI <REEDLUWEI1993@163.com>
2018-12-25 09:18:35 +0800

    2018 12 25 9:17 删除springboot-weather

--- springboot-nanjing/src/main/java/com/nanjing/weather/service/ProductCategoryRegionRelsService.java
+++
@@ -1,9 +0,0 @@
-package com.nanjing.weather.service;
-
-import com.nanjing.weather.domain.ProductCategoryRegionRels;
-
-import java.util.List;
-
-public interface ProductCategoryRegionRelsService {
-    public List<ProductCategoryRegionRels> findByCategoryCode(String categoryCode);
-}
