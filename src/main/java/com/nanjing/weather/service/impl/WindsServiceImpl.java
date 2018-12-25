a9a752aad3b0d915f84e30cd07b45c56311fe142
xiaowuler <xiaowuler@163.com>
2018-12-25 09:43:27 +0800

    12/25 9:42 windService代码规整

--- src/main/java/com/nanjing/weather/service/impl/WindsServiceImpl.java
+++ src/main/java/com/nanjing/weather/service/impl/WindsServiceImpl.java
@@ -117,7 +117,7 @@ public class WindsServiceImpl implements WindsService {
         List<Winds> windsList = new ArrayList<>();
         Integer num = Integer.parseInt(parmsTwo.substring(1));
         //判断前端查询条件
-        if(parmsOne.equals("平均风")){
+        /*if(parmsOne.equals("平均风")){
             WindCenter windCenter = new WindCenter();
             windCenter.setSpeed(new BigDecimal(num));
             windCenter.setCreateTime(time.split(":")[0]);
@@ -205,6 +205,50 @@ public class WindsServiceImpl implements WindsService {
                     }
                 }
             }
+        }*/
+
+        WindCenter windCenter = new WindCenter();
+        windCenter.setSpeed(new BigDecimal(num));
+        windCenter.setCreateTime(time.split(":")[0]);
+        windCenter.setRoutineTime(time.split(":")[1]);
+        winds =WindsMapper.findAllBySomeTerm(windCenter);
+        if(winds.size() > 0){
+            for(Wind wind:winds){
+                double x = 0;
+                double z = 0;
+                double y = 0;
+                for(WindCenter windCenters:wind.getWindCenter()){
+                    if(parmsOne.equals("平均风")){
+                        if(Double.parseDouble(windCenters.getSpeed().toString())<9999 && Double.parseDouble(windCenters.getDirection().toString())<9999){
+                            x += Double.parseDouble(windCenters.getSpeed().toString());
+                            z += Double.parseDouble(windCenters.getDirection().toString());
+                            y++;
+                        }
+                    }else if(parmsOne.equals("2分钟风向风速")){
+                        if(Double.parseDouble(windCenters.getSpeedTwoMin().toString())<9999 && Double.parseDouble(windCenters.getDirectionTwoMin().toString())<9999){
+                            x += Double.parseDouble(windCenters.getSpeedTwoMin().toString());
+                            z += Double.parseDouble(windCenters.getDirectionTwoMin().toString());
+                            y++;
+                        }
+                    }else{
+                        if(Double.parseDouble(windCenters.getSpeedTenMin().toString())<9999 && Double.parseDouble(windCenters.getDirectionTenMin().toString())<9999){
+                            x += Double.parseDouble(windCenters.getSpeedTenMin().toString());
+                            z += Double.parseDouble(windCenters.getDirectionTenMin().toString());
+                            y++;
+                        }
+                    }
+                }
+                Winds windOne = new Winds();
+                if(y != 0){
+                    windOne.setAvg_Speed(new BigDecimal(new DecimalFormat("#.00").format(x/y)));
+                    windOne.setAvg_Speed_Direction(new BigDecimal(new DecimalFormat("#.00").format(z/y)));
+                    //windOne.setAvg_Speed(new BigDecimal(x/y));
+                    //windOne.setAvg_Speed_Direction(new BigDecimal(z/y));
+                    windOne.setStation_Id(wind.getStationId());
+                    windsList.add(windOne);
+                }
+            }
+            //System.out.println(x+"\t"+y+"\t"+z);
         }
 
         //判断查询数据是否可用
