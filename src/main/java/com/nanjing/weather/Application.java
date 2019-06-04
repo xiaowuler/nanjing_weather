37b47ab57c70f8db3fa4a75681ba0b780af20d70
小武 <xiaowuler@163.com>
2019-06-04 17:09:22 +0800

    5/14 17:13 处理数据为空时图片显示

--- src/main/java/com/nanjing/weather/Application.java
+++ src/main/java/com/nanjing/weather/Application.java
@@ -6,9 +6,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 @SpringBootApplication
 @Import(SpringUtil.class)
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
